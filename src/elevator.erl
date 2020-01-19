936120d691b24feeb675effbff745a20acb5dd67
DanielVeB <danielkurosz@gmail.com>
2020-01-19 16:12:25 +0100

    Update elevator.erl
    Add add of logic to manage an elevator

--- src/elevator.erl
+++ src/elevator.erl
@@ -11,7 +11,7 @@
 
 %% API
 
--export([setElevator/0]).
+-compile(export_all).
 
 setElevator() ->
   spawn(fun() -> elevator(1, {1, 6}, 3, 1, [], []) end).
@@ -21,13 +21,16 @@ elevator(ID, {Low, High}, Current_floor, Moving, On_board, Waiting) ->
   receive
 
     {call_elevator, Pedestrian_ID, Pedestrian_floor} ->
-      elevator(ID, {Low, High}, Current_floor, Moving, On_board, Waiting ++ [{Pedestrian_ID, Pedestrian_floor}]);
+      io:format("Passenger with id ~p has called the elevator ~n", [Pedestrian_ID]),
+      W = Waiting ++ [{Pedestrian_ID, Pedestrian_floor}],
+      elevator(ID, {Low, High}, Current_floor, Moving, On_board, W);
 
     {status} ->
-      io:format("==================STATUS==================== ~n"),
-      io:format("Elevator number: ~p, Current position: ~p ~n", [ID, Current_floor]),
-      io:format("Passengers on board: ~p ~n ", [On_board]),
-      io:format("============================================ ~n"),
+      io:format(
+        "==================STATUS====================
+  Elevator number: ~p, Current position: ~p
+  Passengers on board: ~p
+  ============================================ ~n", [ID, Current_floor, On_board]),
       elevator(ID, {Low, High}, Current_floor, Moving, On_board, Waiting);
 
     {choose_floor, Pedestrian_ID, Desired_floor} ->
@@ -39,34 +42,92 @@ elevator(ID, {Low, High}, Current_floor, Moving, On_board, Waiting) ->
           io:format("Elevator ~p, get new passenger on board ~n", [ID]),
           elevator(ID, {Low, High}, Current_floor, Moving, On_board ++ [{Pedestrian_ID, Desired_floor}], Waiting)
       end;
-    {move} ->
-      if (Current_floor + Moving) > High ->
-        io:format("Elevator ~p stopped, top floor : ~p ~n", [ID, High]),
-        elevator(ID, {Low, High}, Current_floor, 0, On_board, Waiting);
-        (Current_floor + Moving) < Low ->
-          io:format("Elevator ~p stopped, lowest floor : ~p ~n", [ID, Low]),
-          elevator(ID, {Low, High}, Low, 0, On_board, Waiting);
-        true ->
-          if Moving == 0 ->
-            io:format("Elevator ~p stopped, lowest floor : ~p ~n", [ID, Low]);
-            true ->
-              io:format("Elevator ~p moving ~n", [ID]),
-              Pedestrians_On_Floor = onFloor(On_board, Current_floor),
-              Length = length(Pedestrians_On_Floor),
-              if
-                Length > 0 ->
-                  call_passangers_exit(Pedestrians_On_Floor),
-                  P = delete_passangers(Pedestrians_On_Floor, Current_floor, On_board),
-                  elevator(ID, {Low, High}, Current_floor + Moving, Moving, P, Waiting);
-                Length == 0 ->
-                  elevator(ID, {Low, High}, Current_floor + Moving, Moving, On_board, Waiting)
-              end
+
+    {next_turn} ->
+      io:format("Elevator ~p moving ~p, current floor ~p
+      On board: ~p Waiting: ~p ~n", [ID, if Moving == 1 -> "TOP"; Moving == -1 -> "DOWN"; true -> "STOP"
+                                                         end, Current_floor, On_board, Waiting]),
+      {LeftOnBoard, LeftWaiting} = update_passangers(self(), Current_floor, On_board, Waiting, {Low, High}),
+      OnBoardLength = length(LeftOnBoard),
+      Waiting_Length = length(LeftWaiting),
+      if
+        OnBoardLength > 0 ->
+          ArePeople = are_people_who_want_to_next_floors(On_board, Current_floor, Moving),
+          if
+            ArePeople == 1 ->
+              elevator(ID, {Low, High}, Current_floor + Moving, Moving, LeftOnBoard, LeftWaiting);
+            ArePeople == 0 ->
+              elevator(ID, {Low, High}, Current_floor - Moving, - Moving, LeftOnBoard, LeftWaiting)
+          end;
+        OnBoardLength == 0 ->
+          if
+            Waiting_Length > 0 ->
+              Direction = setDirection(lists:nth(1, LeftWaiting), Current_floor),
+              elevator(ID, {Low, High}, Current_floor + Direction, Direction, LeftOnBoard, LeftWaiting);
+            Waiting_Length == 0 ->
+              elevator(ID, {Low, High}, Current_floor, 0, LeftOnBoard, LeftWaiting)
           end
-
       end
-
   end.
 
+
+setDirection({Passanger, Floor}, CurrentFloor) ->
+  if
+    Floor > CurrentFloor ->
+      1;
+    Floor < CurrentFloor ->
+      -1
+  end.
+
+%%  Return 1 if there are people on board who want to next floor
+%%  Return 0 if there is empty elevator, or every passanger wants to floor in other direction
+%%
+%%
+are_people_who_want_to_next_floors([{PassangerId, Floor}], CurrentFloor, Moving) ->
+  if
+    Moving == 1 ->
+      if
+        Floor > CurrentFloor -> 1;
+        true -> 0
+      end;
+    Moving == -1 ->
+      if
+        Floor < CurrentFloor -> 1;
+        true -> 0
+      end;
+    Moving == 0 ->
+      1
+  end;
+
+are_people_who_want_to_next_floors([{PassangerId, Floor} | T], CurrentFloor, Moving) ->
+  if
+    Moving == 1 ->
+      if
+        Floor > CurrentFloor -> 1;
+        true -> are_people_who_want_to_next_floors(T, CurrentFloor, Moving)
+      end;
+    Moving == -1 ->
+      if
+        Floor < CurrentFloor -> 1;
+        true -> are_people_who_want_to_next_floors(T, CurrentFloor, Moving)
+      end
+  end.
+
+
+get_passengers_on_this_floor(ElevatorPID, [], Floor, {Min, Max}) ->
+  [];
+
+get_passengers_on_this_floor(ElevatorPID, Waiting, Floor, {Min, Max}) ->
+  PIDS = onFloor(Waiting, Floor),
+  call_passengers(ElevatorPID, PIDS, {Min, Max}),
+  delete_passangers(PIDS, Floor, Waiting).
+
+call_passengers(ElevatorPID, [], {Min, Max}) ->
+  [];
+
+call_passengers(ElevatorPID, [WaitingPid | T], {Min, Max}) ->
+  WaitingPid ! {enter, ElevatorPID, {Min, Max}},
+  call_passengers(ElevatorPID, T, {Min, Max}).
 
 onFloor([], CurrentFloor) ->
   [];
@@ -83,18 +144,34 @@ onFloor([{PassangerPID, Floor} | T], CurrentFloor) ->
 call_passanger_to_exit(PID) ->
   PID ! {exit}.
 
-call_passangers_exit([Pid]) ->
-  io:format("Passanger with ~p called to leave ~n", [Pid]),
-  call_passanger_to_exit(Pid);
+call_passangers_exit([]) ->
+  ok;
 
 call_passangers_exit([Pid | T]) ->
   io:format("Passanger with ~p called to leave ~n", [Pid]),
   call_passanger_to_exit(Pid),
   call_passangers_exit(T).
 
-delete_passangers([PID], Floor, PIDS) ->
-  PIDS -- [{PID, Floor}];
+enter_waiting_passengers(ElevatorPid, [], {Min, Max}) ->
+  [];
+
+enter_waiting_passengers(ElevatorPid, [PID | T], {Min, Max}) ->
+  PID ! {enter, ElevatorPid, {Min, Max}},
+  enter_waiting_passengers(ElevatorPid, T, {Min, Max}).
+
+delete_passangers([], Floor, PIDS) ->
+  PIDS;
 
 delete_passangers([PID | T], Floor, PIDS) ->
   NewPids = PIDS -- [{PID, Floor}],
   delete_passangers(T, Floor, NewPids).
+
+update_passangers(ElevatorPID, CurrentFloor, OnBoard, Waiting, {Min, Max}) ->
+  WaitingOnThisFloor = onFloor(Waiting, CurrentFloor),
+  enter_waiting_passengers(ElevatorPID, WaitingOnThisFloor, {Min, Max}),
+  LeftWaiting = delete_passangers(WaitingOnThisFloor, CurrentFloor, Waiting),
+
+  PeopleWhoWantToLeave = onFloor(OnBoard, CurrentFloor),
+  call_passangers_exit(PeopleWhoWantToLeave),
+  LeftOnBoard = delete_passangers(PeopleWhoWantToLeave, CurrentFloor, OnBoard),
+  {LeftOnBoard, LeftWaiting}.
--- src/main.erl
+++ src/main.erl
@@ -10,59 +10,44 @@
 -author("daniel").
 
 %% API
--import(elevator,[setElevator/0]).
+-import(elevator,[setElevator/0, elevator/6]).
 -compile(export_all).
-
-%%call_elevator(ElevatorPID, {CurrentFloor, DesiredFloor}, {MinFloor, MaxFloor}) ->
-%%  if CurrentFloor =< MaxFloor and CurrentFloor >= MinFloor and DesiredFloor =< MaxFloor and DesiredFloor >= MinFloor ->
-%%    ElevatorPID ! {pedestrian, CurrentFloor, DesiredFloor}
-%%  end.
-
-
-
-
 
 setPassanger() ->
   spawn(fun() -> pedestrian() end).
 
 pedestrian() ->
   receive
-    {enter, ElevatorPID} ->
-      io:format("Pedestrian with id: ~p enter to elevator with id: ~p", [self(), ElevatorPID]),
-      ElevatorPID ! {choose_floor, self(), 7};
+    {enter, ElevatorPID, {Min, Max}} ->
+      io:format("Passenger with id: ~p has entered to elevator ~n", [self()]),
+      ElevatorPID ! {choose_floor, self(), rand:uniform(Max - Min) + Min},
+      pedestrian();
     {exit} ->
-      io:format("Pedestrian with id: ~p has left the elevator ~n", [self()])
+      io:format("Passenger with id: ~p has left the elevator ~n", [self()])
   end.
 
-elevator1({Low, High}, Current_floor, ID, Moving, Direction) ->
-  receive
-    {pedestrian, PCurrent_Floor, PDesired_Floor} ->
-      io:fwrite("Elevator ~p Current floor ~p ~n", [ID, Current_floor]),
-      io:fwrite("Pedestrian on flooor ~p wants to floor ~p ~n", [PCurrent_Floor, PDesired_Floor]),
-      elevator1({Low, High}, Current_floor, ID, Moving, Direction);
-    {status} ->
-      io:format("Elevator number: ~p, Current position: ~p ~n", [ID, Current_floor]),
-      elevator1({Low, High}, Current_floor, ID, Moving, Direction);
-    {move} ->
-      if Moving == 1 ->
-        io:format("Elevator number: ~p, Moving~n", [ID]),
-        elevator1({Low, High}, Current_floor + Direction, ID, Moving, Direction)
-      end;
-    Other ->
-      elevator1({Low, High}, Current_floor, ID, Moving, Direction)
-  end.
-
-
-%%elevator(Moving, Direction, Current_Floor, Next_Floor)
-
-new_elevator({Low, High}, ID) ->
-  spawn(fun() -> elevator1({Low, High}, 0, ID, 1, 1) end).
 
 getPID(PIDS, Number) ->
   list_to_pid(lists:flatten(io_lib:format("~p", [lists:nth(Number, PIDS)]))).
 
 getPID(PID) ->
   io_lib:format("~p", PID).
+
+getStatus([]) ->
+  ok;
+
+getStatus([PID | T]) ->
+%%  PID ! {status},
+  PID ! {next_turn},
+  getStatus(T).
+
+status(PIDs) ->
+  getStatus(PIDs).
+
+
+new_elevator({Min_Floor, Max_Floor}, N) ->
+  Init_Floor = rand:uniform(Max_Floor - Min_Floor) + Min_Floor,
+  spawn(fun() -> elevator(N, {Min_Floor, Max_Floor}, Init_Floor, 0, [], []) end).
 
 setElevator({Min_Floor, Max_Floor}, 0) ->
   [];
@@ -71,33 +56,21 @@ setElevator({Min_Floor, Max_Floor}, N) ->
   PID = new_elevator({Min_Floor, Max_Floor}, N),
   [PID] ++ setElevator({Min_Floor, Max_Floor}, N - 1).
 
-start({MinFloor, MaxFloor}, Number_of_elevators) ->
-  setElevator({MinFloor, MaxFloor}, Number_of_elevators).
+sim({Min, Max}, N) ->
+  PIDS = setElevator({Min,Max}, N),
+  register_passengers(PIDS, {Min, Max}, N).
 
-getStatus([PID]) ->
-  PID ! {status};
-
-getStatus([PID | T]) ->
-  PID ! {status},
-  getStatus(T).
-
-status(PIDs) ->
-  getStatus(PIDs).
+sim1() ->
+  sim({0,10},1).
 
 
-simulation() ->
-  El = setElevator(),
-  P1 = setPassanger(),
-  P2 = setPassanger(),
-  P3 = setPassanger(),
-  P4 = setPassanger(),
-  El ! {status},
-  El ! {choose_floor, P1, 6},
-  El ! {choose_floor, P2, 4},
-  El ! {choose_floor, P3, 4},
-  El ! {choose_floor, P4, 4},
-  El ! {status},
-  El ! {move},
-  El ! {status},
-  El ! {move},
-  El ! {status}.
+register_passengers(PIDS, {Min, Max}, N) ->
+  ElPID = lists:nth(rand:uniform(N), PIDS),
+  Passenger = setPassanger(),
+  ElPID ! {call_elevator, Passenger, rand:uniform(Max - Min) + Min},
+  timer:sleep(5000),
+  getStatus(PIDS),
+  register_passengers(PIDS, {Min, Max}, N ).
+
+
+
